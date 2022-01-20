6c9aafbef2e59a10db1cf84e62b87e2373701af4
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-01-20 18:49:45 +0530

    Time: 20 ms (83.76%), Space: 19.6 MB (42.91%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& b) {
+        for(int i=0;i<9;i++)
+        {
+            vector<int> fre(10);
+            for(int j=0;j<9;j++)
+            {
+                if(b[i][j]=='.')
+                    continue;
+                if(fre[b[i][j]-'0']>0)
+                    return false;
+                fre[b[i][j]-'0']++;
+            }
+        }
+        for(int i=0;i<9;i++)
+        {
+            vector<int> fre(10);
+            for(int j=0;j<9;j++)
+            {
+                if(b[j][i]=='.')
+                    continue;
+                if(fre[b[j][i]-'0']>0)
+                    return false;
+                fre[b[j][i]-'0']++;
+            }
+        }
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                vector<int> fre(10);
+                int si = (i / 3) * 3;
+	            int sj = (j / 3) * 3;
+                for (int k = si ; k < si + 3; k++) 
+                {
+		            for (int l = sj; l < sj + 3; l++) 
+                    {
+			            if(b[k][l]=='.')
+                            continue;
+                        if(fre[b[k][l]-'0']>0)
+                            return false;
+                        fre[b[k][l]-'0']++;
+	                }
+	            }   
+            }
+        }
+        return true;
+    }
+};
