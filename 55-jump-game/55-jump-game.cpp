6e8f2495540c577f964e64c45442690292d046d8
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-01-22 00:12:13 +0530

    Time: 85 ms (49.89%), Space: 48.2 MB (94.63%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int lastidx=n-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(i+nums[i]>=lastidx)
+                lastidx=i;
+        }
+        return lastidx==0;    
+    }
+};
