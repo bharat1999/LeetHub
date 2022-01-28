bbfc459e364f8469e941bdba57ea1e2a94446fb1
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-01-29 00:32:54 +0530

    Time: 157 ms (28.60%), Space: 40.8 MB (87.84%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,71 +1,85 @@
-class Trie
-{
+class Node 
+{    
     public:
-    bool isEnd;
-    unordered_map<char,Trie*> mp;
-    Trie()
+        unordered_map<char,Node*> children;
+        bool isEnd;
+        Node()
+        {
+            isEnd = false;
+        }
+        // Helper function to create a child node if not present and goto it
+        Node* gotoChild(char ch) 
+        {
+            if(children.count(ch) == 0)
+                children[ch] = new Node();
+            return children[ch];
+        }
+        // Helper function to check if ch is child of current node or not
+        bool hasChild(char ch) {
+            return children.count(ch) > 0;
+        }
+};
+
+class Trie 
+{    
+    Node* root = NULL;
+    bool search(Node* root,string &s, int i) 
     {
-        isEnd = false;
+        // If we checked whole string check if cur node is an ending point of word or not
+        if(i == s.size()) 
+            return root->isEnd;
+        // If current character is not . check if cur node have ch as child or not
+        // is its their recursively search by going to ch node and next char of string to be
+        // searched if not return false
+        if(s[i]!= '.') 
+        {    
+            if(root -> hasChild(s[i])) 
+                return search(root -> gotoChild(s[i]), s, i + 1);
+            else
+                return false;
+        }
+        // If current character is . than check for all possible children node of current node
+        // Set ans to false initially and OR it with every possible answer if any one if true
+        // Our answer will be true
+        // So we will search in child node of current with index i + 1
+        bool ans = false;
+        for(auto &itr : root -> children) 
+            ans |= search(itr.second, s, i + 1);
+        return ans;
+    }
+    
+public:
+    Trie() 
+    {
+       root = new Node(); 
+    }
+    void insert(string &s) 
+    {    
+        auto cur = root;
+        for(auto &ch : s) 
+            cur  = cur->gotoChild(ch);
+        cur -> isEnd = true;
+    }
+    bool search(string &s) 
+    {
+        return search(root, s, 0);
     }
 };
 
-class WordDictionary 
-{
-    Trie *root = new Trie();
-    bool search(string s,Trie *root)
-    {
-        auto *cur = root;
-        int n = s.size();
-        for(int i=0;i<n;i++)
-        {
-            char c = s[i];
-            if(c!='.')
-            {
-                if(cur->mp.count(c)==0)
-                    return false;
-                else
-                    cur = cur->mp[c];
-            }
-            else
-            {
-                if(cur->mp.empty())
-                    return false;
-                for(auto x:cur->mp)
-                {
-                    cur = x.second;
-                    if(search(s.substr(i+1),cur))
-                        return true;
-                }
-            }
-        }
-        return cur->isEnd;
-    }   
+
+class WordDictionary {
+    Trie t;
 public:
-    
     WordDictionary() {
         
     }
     
-    void addWord(string word) 
-    {
-        auto *cur = root;
-        for(auto c:word)
-        {
-            if(cur->mp.count(c)==0)
-                cur->mp[c] = new Trie();
-            cur = cur->mp[c];
-        }
-        cur->isEnd = true;
+    void addWord(string word) {
+        t.insert(word);
     }
     
     bool search(string word) {
-        return search(word,root);
+        return t.search(word);
     }
+    
 };
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary* obj = new WordDictionary();
- * obj->addWord(word);
- * bool param_2 = obj->search(word);
- */
