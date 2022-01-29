094f599da38c1d8de720f2a37b058e21bff66e94
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-01-30 00:22:23 +0530

    Time: 3 ms (18.33%), Space: 5.9 MB (53.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    
-    int stairs(int n,int *dp)
-    {
-        if(n==1 or n==2)
-        {
-            return dp[n]=n;
-        }
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n]=stairs(n-1,dp)+stairs(n-2,dp);
-    }    
 public:
     int climbStairs(int n) {
-        int dp[46];
-        dp[1]=1;
-        dp[2]=2;
+        if(n==1 or n==2)
+            return n;
+        int f=1;
+        int s=2;
+        int ans;
         for(int i=3;i<=n;i++)
-            dp[i]= dp[i-1] + dp[i-2]; // way to reach n form n-1 and n-2 th stair
-        return dp[n];
+        {
+            ans = f + s;
+            f = s;
+            s = ans;
+        }
+        return ans;
     }
 };
