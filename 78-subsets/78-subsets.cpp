839214e405697384289af824040bb6d83989d4c1
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-02 00:24:46 +0530

    Time: 5 ms (27.61%), Space: 7 MB (85.03%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int no=0; no < (1<<n); no++) 
+        {// for each no, iterate over its n-bit binary representation from right to left
+            vector<int> tmp;
+		    for(int i=0; i<n; i++) 
+            {
+			    // if the ith bit is set, include the element at the ith index in the subseq.
+			    if((1<<i)&no)
+                    tmp.push_back(nums[i]);
+			}
+		    ans.push_back(tmp);
+	    }
+        return ans;
+    }
+};
