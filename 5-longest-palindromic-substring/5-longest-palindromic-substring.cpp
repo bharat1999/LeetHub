daf7f147ac9050e566cb7f7a1cee3cbe7269e2bf
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-03 23:36:08 +0530

    Time: 506 ms (22.02%), Space: 47.3 MB (29.84%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    bool isPallindrome(string s)
+    {
+        int i=0;
+        int e=s.size()-1;
+        while(i<e)
+        {
+            if(s[i]!=s[e])
+                return false;
+            i++;
+            e--;
+        }
+        return true;
+    }
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        if(n==1)
+            return s;
+        vector<vector<bool>> dp(n,vector<bool> (n,false));
+        // as string starting and ending at same index is always //pallindrome
+        for(int i=0;i<n;i++)
+            dp[i][i]=true;
+        // checking if 2 size substring is pallindrome or not
+        for(int i=0;i<n-1;i++)
+            dp[i][i+1] = s[i]==s[i+1];
+        string ans="";
+        // single element will always be the answer
+        ans.push_back(s[0]);
+        int maxLen = 1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(s[i]==s[j])
+                {
+                    if(j-i==1 or dp[i+1][j-1])
+                    {
+                        dp[i][j]=true;
+                        if(j-i+1>maxLen)
+                        {
+                            maxLen = j-i+1;
+                            ans = s.substr(i,j-i+1);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
