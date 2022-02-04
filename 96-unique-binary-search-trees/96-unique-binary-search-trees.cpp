438e2b6669f277ac9654ff993d84da43414618cb
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-04 15:32:32 +0530

    Time: 3 ms (21.44%), Space: 6 MB (65.80%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,11 +1,16 @@
 class Solution {
+    int dp[20]={};
 public:
-    int dp[20]{};
     int numTrees(int n) {
-        if(n <= 1) return 1;
-        if(dp[n]) return dp[n];
-        for(int i = 1; i <= n; i++) 
-            dp[n] += numTrees(i-1) * numTrees(n-i);
-        return dp[n];
+        if(n==1 or n==0)
+            return dp[n]=1;
+        if(dp[n])
+            return dp[n];
+        int ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            ans+=numTrees(i-1)*numTrees(n-i);
+        }
+        return dp[n]=ans;
     }
 };
