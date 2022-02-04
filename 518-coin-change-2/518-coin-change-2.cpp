8dd4c970e6f0426d2533e5aa3e0254037219a2a7
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-04 15:18:25 +0530

    Time: 16 ms (72.32%), Space: 12 MB (43.19%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,26 +1,27 @@
 class Solution {
-    int solve(vector<int>& coins,int n,int amount,vector<vector<int>> &dp)
-    {
-        if(n==0)
-            return 0;
-        if(amount == 0)
-            return dp[n][amount]=1;
-        if(dp[n][amount]!=-1)
-            return dp[n][amount];
-        if(coins[n-1] > amount)
-        {
-            return dp[n][amount]=solve(coins, n-1, amount,dp);
-        }
-        return dp[n][amount]=solve(coins, n, amount-coins[n-1],dp) + solve(coins, n-1, amount,dp); 
-    }
 public:
+    
+    int change(vector<int> coins,int amount,int size)
+    {
+        if(size==0)
+            return 1;  
+        int dp[size][amount+1];
+        for(int i=0;i<size;i++)
+            dp[i][0]=1;
+        for(int i=0;i<size;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                int x = i-1>=0 ? dp[i-1][j]:0; // ek baar coin nhi lia
+                int y = j-coins[i]>=0?dp[i][j-coins[i]]:0; // ek baar le lia
+                dp[i][j]=x+y;   
+            }
+        }   
+        return dp[size-1][amount]; // ek baad last wala lege ek baar nahi
+}
+
+    
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
-        vector<vector<int>> dp(n+2,vector<int> (amount+2,-1));
-        if(amount==0)
-            return 1;
-        if(n==0)
-            return 0;
-        return solve(coins,n,amount,dp);
+        return change(coins,amount,coins.size());
     }
 };
