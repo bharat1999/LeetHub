051d46fff732f65ee76030e404969790fe24593d
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-04 15:52:23 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.50%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int nthUglyNumber(int n) 
-    {    
+    int nthUglyNumber(int n) {
         int dp[n+1];
-        dp[1] = 1;
-        int idx2 = 1, idx3 = 1, idx5 = 1;
-        for(int i=2; i<=n; i++)
-        {   
-            // we have to find all 3 next ugly number and take its number
-            dp[i] = min(2*dp[idx2],min(3*dp[idx3],5*dp[idx5]));
-            // whicherver index number become dp[i] we increment that index
-            if(dp[i] == 2*dp[idx2])
+        dp[1]=1;
+        int idx2 =1 ,idx3=1,idx5 =1;
+        for(int i=2;i<=n;i++)
+        {
+            long long val2 = 2*dp[idx2];
+            long long val3 = 3*dp[idx3];
+            long long val5 = 5*dp[idx5];
+            long long mi = min(val2,min(val3,val5));
+            dp[i]=mi;
+            if(mi == val2)
                 idx2++;
-            if(dp[i] == 3*dp[idx3])
+            if(mi == val3)
                 idx3++;
-            if(dp[i] == 5*dp[idx5])
+            if(mi==val5)
                 idx5++;
         }
         return dp[n];
-        
     }
 };
