effdabbaf12ef451464d7f5b1deeb5b454926fa4
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-06 22:53:38 +0530

    Time: 4 ms (86.50%), Space: 10.9 MB (74.56%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
-        if(n<3)
+        if(n<=2) 
             return n;
-        int i=2;
-        for(int j=i;j<n;j++)
+        int count=1,idx=1;
+        for(int i=1;i<n;i++)
         {
-            // we got the number to replace
-            if(nums[j]!=nums[i-2])
-                nums[i++]=nums[j];
+            // same number so increase count
+            if(nums[i-1]==nums[i]) 
+                count++;
+            // new number so make count 1
+            else count=1;
+            //count is less than equal to 2 than its valid to put ith value at idx
+            if(count<=2) nums[idx++]=nums[i];
         }
-        return i;
+        return idx;
     }
 };
