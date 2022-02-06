ee43c1fa839aa85cf2e99248516ca1e79978647c
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-06 23:17:09 +0530

    Time: 145 ms (33.48%), Space: 11.1 MB (74.82%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mincoin(vector<int> a,int n,int m)
+    {
+        if(n==0)
+            return 0;  
+        long long int dp[m][n+1];
+        for(int i=0;i<m;i++)
+            dp[i][0]=0;
+        for(int i=0;i<m;i++)
+        {
+             for(int j=1;j<=n;j++)
+            {
+                long long int x=INT_MAX,y=INT_MAX;
+                x = i-1>=0 ? dp[i-1][j]:INT_MAX; // ek baar coin nhi lia
+                y = j-a[i]>=0?1+dp[i][j-a[i]]:INT_MAX; // ek baar le lia
+                dp[i][j]=min(x,y);   
+            }
+        }   
+        return dp[m-1][n]; // ek baad last wala lege ek baar nahi
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        long long int ans =mincoin(coins,amount,coins.size());
+        if(ans==INT_MAX)
+            return -1;
+        return ans;
+    }
+};
