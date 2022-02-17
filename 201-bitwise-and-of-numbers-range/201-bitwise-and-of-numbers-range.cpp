5ed3cc21054259a108cf3d77c7445cbd9eaa1f8f
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-18 00:05:16 +0530

    Time: 12 ms (65.81%), Space: 6 MB (34.41%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -5,14 +5,13 @@ public:
             return 0;
         if(left == right)
             return left;
-        if(right-left==1)
-            return right&left;
+        //if(right-left==1)
+            //return right&left;
         int leftmsb=log2(left);
         int rightmsb=log2(right);
         if(leftmsb<rightmsb)
             return 0;
         int ans=0;
-        cout<<leftmsb;
         for(int i=leftmsb;i>=0;i--)
         {
             if(((1<<i)&left)!=((1<<i)&right))
