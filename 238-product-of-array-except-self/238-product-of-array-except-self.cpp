b50ef77ced6cd39a7b6c4af02e786f690a4e0210
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-22 23:20:17 +0530

    Time: 26 ms (71.32%), Space: 25.2 MB (14.42%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,20 +3,22 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> prefix(n),suffix(n),res(n);
-        // for first element prefix have no elements
-        prefix[0]=1;
-        // for last element suffix have no elements considering 0 as last
-        suffix[0]=1;
+        // for first element prefix is that element itself
+        prefix[0]=nums[0];
+        // for last element suffix product is that element itself
+        suffix[n-1]=nums[n-1];
         for(int i=1;i<n;i++)
         {
-            // as prefix[i-1] have product except i-1th element
-            prefix[i]=prefix[i-1]*nums[i-1];
-            // prefix[i-1] have product of all from n-i+1 till n-1
+            // as prefix[i-1] have product except ith element
+            prefix[i]=prefix[i-1]*nums[i];
+            // prefix[i-1] have product of all 
             // so include 
-            suffix[i]=suffix[i-1]*nums[n-i];
+            suffix[n-i-1]=suffix[n-i]*nums[n-i-1];
         }
-        for(int i=0;i<n;i++)
-            res[i]=prefix[i]*suffix[n-i-1];
+        res[0]=suffix[1];
+        res[n-1]=prefix[n-2];
+        for(int i=1;i<n-1;i++)
+            res[i]=prefix[i-1]*suffix[i+1];
         return res;
     }
 };
