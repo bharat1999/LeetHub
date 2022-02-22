5a799196ab73532fe0420239f1c15ca1160e1cea
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-22 22:54:04 +0530

    Time: 145 ms (29.00%), Space: 41.7 MB (27.28%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -3,12 +3,14 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         unordered_map<int,int> prefix;
+        // their will always we an array having 0 sum that is empty
         prefix[0]++;
         int count=0;
         int sum=0;
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
+            // we have to add the number of times that sum exist already
             count+=prefix[sum-k];
             prefix[sum]++;
         }
