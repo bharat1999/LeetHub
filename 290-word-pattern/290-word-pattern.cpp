56769143296cfb9c048e3bd255dab13719de374e
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-02-24 21:46:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (95.57%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
     bool wordPattern(string pattern, string s) {
         int n=pattern.size();
+        // pattern char to string map
         unordered_map<char,string> ptos;
+        // strung to pattern car map;
         unordered_map<string,char> stop;
+        // to store words as seperate strings
         vector<string> words;
         string tmp="";
         for(auto x:s)
@@ -18,15 +21,21 @@ public:
         }
         words.push_back(tmp);
         int m=words.size();
+        // if no of words not equal to pattern length its false
         if(m!=n)
             return false;
         for(int i=0;i<n;i++)
         {
+            // if we have a char already attached to a string or
+            // a string already attched to a pattern
             if(ptos.find(pattern[i])!=ptos.end() or stop.find(words[i])!=stop.end())
             {
+                // check if currrent char is same as prev matched with same string or
+                // current string is same as prev matched with same char
                 if(ptos[pattern[i]]!=words[i] or stop[words[i]]!=pattern[i])
                     return false;
             }
+            // if not present add to map
             else
             {
                 ptos[pattern[i]]=words[i];
