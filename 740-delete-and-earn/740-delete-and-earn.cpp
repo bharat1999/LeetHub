5d76ffbc9ea0090b214e25e9b4b5991b0e3c3756
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-03-05 23:41:17 +0530

    Time: 20 ms (23.62%), Space: 14 MB (15.60%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int deleteAndEarn(vector<int>& nums) 
-    {
-        int n = 10001; // as max value of nums[i] = 10000
-        int maxi=*max_element(nums.begin(),nums.end()); // so that loop will only run till required
-        vector<int> sum(n, 0);
-        vector<int> dp(n, 0);
-        for(auto num: nums)
-            sum[num] += num;
+    int deleteAndEarn(vector<int>& nums) {
+        int n = 10001;
+        vector<int> sum(n,0);
+        vector<int> dp(n,0);
+        for(auto x:nums)
+            sum[x]+=x;
         dp[0] = 0;
         dp[1] = sum[1];
-        for(int i=2; i<=maxi; i++)
-            dp[i] = max(dp[i-2] + sum[i], dp[i-1]);
-        return dp[maxi];
+        for(int i=2;i<n;i++)
+        {
+            // we either take current and one before adjacent or take adjacent one
+            dp[i] = max(sum[i] + dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
