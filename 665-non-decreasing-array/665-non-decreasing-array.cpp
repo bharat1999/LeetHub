dc5335a96a9eedca7f225e1a4c9ef9400f2e36e8
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-06-25 22:21:29 +0530

    Time: 45 ms (37.63%), Space: 26.8 MB (97.48%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int c = 0;
+        int idx = -1;
         int n = nums.size();
         for(int i=0;i<n-1;i++)
         {
             if(nums[i]>nums[i+1])
             {
-                c++;
+                if(idx!=-1)
+                    return false;
+                idx=i;
             }
         }
-        if(c>1)
-            return false;
-        for(int i=0;i<n-1;i++)
-        {
-            // check is replacement possible
-            if(nums[i]>nums[i+1])
-            {
-                if(i==0)
-                    nums[i]=nums[i+1];
-                else if(i==n-2)
-                    nums[i+1]=nums[i];
-                else
-                {
-                    if(nums[i-1]<=nums[i+1])
-                        nums[i]=nums[i+1];
-                    else if(nums[i]<nums[i+2])
-                        nums[i+1]=nums[i];
-                    else 
-                        return false;
-                }
-            }
-        }
-        return true;
+        if(idx==-1 or idx==0 or idx==n-2 or nums[idx-1]<=nums[idx+1] or nums[idx]<=nums[idx+2])
+            return true;
+        return false;
     }
 };
