cb3ef42a56becab71f81bd1e3d7045677dbc6d8a
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-07-16 23:29:49 +0530

    Time: 9 ms (79.68%), Space: 6.3 MB (99.01%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
     long long mod = 1e9+7;
-    vector<vector<vector<int>>> dp;
+    int dp[51][51][51];
     int dfs(int &m,int &n,int i,int j,int moveLeft)
     {
         if(i<0 or j<0 or i>=m or j>=n)
@@ -16,7 +16,7 @@ class Solution {
     }
 public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        dp.resize(m+1,vector<vector<int>> (n+1,vector<int> (maxMove+1,-1)));
+        memset(dp,-1,sizeof(dp));
         return dfs(m,n,startRow,startColumn,maxMove);
     }
 };
