826a291fcc3b001a5bbef68e10f54d9113b6af16
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-07-24 00:03:27 +0530

    Time: 1377 ms (19.49%), Space: 150.7 MB (56.37%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,62 +1,22 @@
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>
+#include <functional> // for less
+#include <iostream>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag,
+             tree_order_statistics_node_update> ordered_set;
+ 
 class Solution {
-    vector<int> count;
-    vector<pair<int,int>> temp;
 public:
-
-    void merge(int left, int mid, int right, vector<pair<int, int>>& arr)
-    {        
-        int i = left;
-        int j = mid + 1;
-        int k = left;
-        
-        while(i <= mid and j <= right)
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+        ordered_set s;
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            // putting in descending order so we can calculate right smaller easily
-            if(arr[i].first <= arr[j].first)
-                temp[k++] = arr[j++]; 
-            // as j<i so all right to j will be small
-            // so ans is r  - j +1
-            else
-            {
-                count[arr[i].second] += (right - j +1);
-                temp[k++] = arr[i++];
-            }
+            s.insert(nums[i]);
+            ans[i] = s.order_of_key(nums[i]);
         }
-        
-        while(i <= mid)
-            temp[k++] = arr[i++];
-        
-        while(j <= right)
-            temp[k++] = arr[j++];
-        
-        for(int l = left; l <= right; l++)
-            arr[l] = temp[l];
-        
+        return ans;
     }
-                
-    void mergeSort(int left, int right, vector<pair<int, int>>& arr)
-    {
-        if(left >= right)
-            return;
-
-        int mid = left + (right - left) / 2;
-        
-        mergeSort(left, mid, arr);
-        mergeSort(mid + 1, right, arr);
-        
-        merge(left, mid, right, arr);
-    }
-    
-	vector<int> countSmaller(vector<int>& nums) {
-	    
-        int n=nums.size();
-        count.resize(n);
-        temp.resize(n);
-	    vector<pair<int, int>> arr;
-	    for(int i = 0; i < n; i++)
-	        arr.push_back({nums[i], i});
-	    mergeSort(0, n - 1, arr);
-	    return count;
-	}
-  
 };
