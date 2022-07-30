1fcd05cd2ceecafe5fa4d421e60b7363c0b5960f
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-07-30 22:49:22 +0530

    Time: 479 ms (28.32%), Space: 102.4 MB (65.17%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> ans;
-        unordered_map<char,int> freq;
+        vector<int> freq(26);
         for(auto x:words2)
         {
-            unordered_map<char,int> tmp;
+            vector<int> tmp(26);
             for(auto c:x)
-                tmp[c]++;
-            for(auto y:tmp)
-                freq[y.first] = max(freq[y.first],y.second);
+                tmp[c-'a']++;
+            for(int i=0;i<26;i++)
+                freq[i] = max(freq[i],tmp[i]);
         }
         for(auto x:words1)
         {
-            unordered_map<char,int> tmp2;
+            vector<int> tmp2(26);
             for(auto c:x)
-                tmp2[c]++;
+                tmp2[c-'a']++;
             bool f = true;
-            for(auto y:freq)
+            for(int i=0;i<26;i++)
             {
-                if(tmp2[y.first]<y.second)
+                if(tmp2[i]<freq[i])
                 {
                     f = false;
                     break;
