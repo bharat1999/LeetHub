b32ac5eddaafe28fd3cee558896c534f3f50a5b0
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-08-02 23:44:03 +0530

    Time: 43 ms (74.15%), Space: 13.1 MB (92.86%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,25 @@
 class Solution {
-public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                pq.push(matrix[i][j]);
-                if(pq.size()>k) pq.pop();
-            }
+    int countLessOrEqual(vector<vector<int>>& matrix, int x,int n) 
+    {
+        int cnt = 0, c = n - 1; 
+        for (int r = 0; r < n; ++r) {
+            while (c >= 0 && matrix[r][c] > x) --c;  
+            cnt += (c + 1);
         }
-        return pq.top();
+        return cnt;
     }
+    public:
+ int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size(); 
+        int left = matrix[0][0], right = matrix[n-1][n-1], ans = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (countLessOrEqual(matrix, mid,n) >= k) {
+                ans = mid;
+                right = mid - 1; // try to looking for a smaller value in the left side
+            } else left = mid + 1; // try to looking for a bigger value in the right side
+        }
+        return ans;
+    }
+    
 };
