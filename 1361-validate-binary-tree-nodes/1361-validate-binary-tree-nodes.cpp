abf631e74898a5dd07f183620d461fe60ea54fff
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-08-04 01:31:20 +0530

    Time: 58 ms (83.16%), Space: 32 MB (93.08%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,45 +1,50 @@
 class Solution {
+    vector<int> parent;
+    int find(int p)
+    {
+        if(parent[p]==p)
+            return p;
+        return parent[p] = find(parent[p]);
+    }
+    bool Union(int a,int b)
+    {
+        int pa = find(a);
+        int pb = find(b);
+        // if b already have a parent means it can be access by more than one node so return false
+        if(pb!=b)
+            return false;
+        // if child and parent have same parent means it have a back edge means cycle
+        if(pa==pb)
+            return false;
+        // otherwise set paret of child as i
+        parent[b] = pa;
+        return true;
+    }
 public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        vector<int> inDegree(n,0);
+        parent.resize(n);
+        for(int i=0;i<n;i++)
+            parent[i]=i;
         for(int i=0;i<n;i++)
         {
             if(leftChild[i]!=-1)
-                inDegree[leftChild[i]]++;
+            {
+                if(!Union(i,leftChild[i]))
+                    return false;
+            }
             if(rightChild[i]!=-1)
-                inDegree[rightChild[i]]++;
+            {
+                if(!Union(i,rightChild[i]))
+                    return false;
+            }
         }
-        vector<int> topo;
-        queue<int> q;
+        int tree = 0;
+        // now we will calculate number of tree
         for(int i=0;i<n;i++)
-            if(inDegree[i]==0)
-                q.push(i);
-        // more than one tree
-        if(q.size()!=1)
-            return false;
-        while(!q.empty())
-        {
-            int cur = q.front();
-            q.pop();
-            if(leftChild[cur]!=-1)
-            {
-                inDegree[leftChild[cur]]--;
-                if(inDegree[leftChild[cur]]<0)
-                    return false;
-                q.push(leftChild[cur]);
-            }
-            if(rightChild[cur]!=-1)
-            {
-                inDegree[rightChild[cur]]--;
-                if(inDegree[rightChild[cur]]<0)
-                    return false;
-                q.push(rightChild[cur]);
-            }
-            topo.push_back(cur);
-        }
-        // if topo order have n nodes mean only one tree
-        if(topo.size()==n)
-            return true;
-        return false;
+            if(parent[i]==i)
+                tree++;
+        // if more than one node is parent of itself means more than 1 tree
+        return tree==1;
+        
     }
 };
