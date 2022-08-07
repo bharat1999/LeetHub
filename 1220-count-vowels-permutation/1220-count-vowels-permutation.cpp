28e9e7e94c7134ff5a4c2da92ac2d319a6e13ba7
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-08-08 02:29:24 +0530

    Time: 71 ms (56.39%), Space: 8.2 MB (67.60%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,71 +1,41 @@
 class Solution {
     int mod = 1e9 + 7;
-    int dp[26][20001];
-    int solve(char last,int l,int n)
+    // considering a = 0 till u = 4
+    int dp[5][20001];
+    int solve(int last,int l,int n)
     {
         if(l==n)
         {
             return 1;
         }
-        if(l!=0 and dp[last-'a'][l]!=-1)
-            return dp[last-'a'][l];
+        if(l!=0 and dp[last][l]!=-1)
+            return dp[last][l];
         int ans = 0;
-        if(l==0)
+        for(int i=0;i<5;i++)
         {
-            ans+=solve('a',l+1,n)%mod;
-            ans%=mod;
-            ans+=solve('e',l+1,n)%mod;
-            ans%=mod;
-            ans+=solve('i',l+1,n)%mod;
-            ans%=mod;
-            ans+=solve('o',l+1,n)%mod;
-            ans%=mod;
-            ans+=solve('u',l+1,n)%mod;
-            ans%=mod;
+            // is last was a and current is e
+            if(last==0 and i==1)
+                ans=((ans%mod) + (solve(i,l+1,n)%mod))%mod;
+            // last was e and current is a or i
+            else if(last==1 and (i==0 or i==2))
+                ans=((ans%mod) + (solve(i,l+1,n)%mod))%mod;
+            // last was i and current is not i
+            else if(last==2 and i!=2)
+                ans=((ans%mod) + (solve(i,l+1,n)%mod))%mod;
+            // last is o and current is i or u
+            else if(last==3 and (i==2 or i==4))
+                ans=((ans%mod) + (solve(i,l+1,n)%mod))%mod;
+            else if(last==4 and i==0)                
+                ans=((ans%mod) + (solve(i,l+1,n)%mod))%mod;
         }
-        else
-        {
-            if(last=='a')
-            {
-                ans+=solve('e',l+1,n)%mod;
-                ans%=mod;
-            }
-            else if(last=='e')
-            {
-                ans+=solve('a',l+1,n)%mod;
-                ans%=mod;
-                ans+=solve('i',l+1,n)%mod;
-                ans%=mod;
-            }
-            else if(last=='i')
-            {
-                ans+=solve('a',l+1,n)%mod;
-                ans%=mod;
-                ans+=solve('e',l+1,n)%mod;
-                ans%=mod;
-                ans+=solve('o',l+1,n)%mod;
-                ans%=mod;
-                ans+=solve('u',l+1,n)%mod;
-                ans%=mod;
-            }
-            else if(last=='o')
-            {
-                ans+=solve('i',l+1,n)%mod;
-                ans%=mod;
-                ans+=solve('u',l+1,n)%mod;
-                ans%=mod;
-            }
-            else if(last=='u')
-            {
-                ans+=solve('a',l+1,n)%mod;
-                ans%=mod;
-            }
-        }
-        return dp[last-'a'][l] = ans%mod;
+        return dp[last][l] = ans%mod;
     }
 public:
     int countVowelPermutation(int n) {
         memset(dp,-1,sizeof(dp));
-        return solve('z',0,n);
+        int ans = 0;
+        for(int i=0;i<5;i++)
+            ans = ((ans%mod) + (solve(i,1,n)%mod))%mod;
+        return ans;
     }
 };
