d7e5d4e921fb61345c2f199f4f3a387d64e820b3
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-08-26 23:00:07 +0530

    Time: 3 ms (73.16%), Space: 6.3 MB (45.48%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,47 +1,24 @@
 class Solution {
-public:
-    bool reorderedPowerOf2(int n) {
-        unordered_map<int,int> dig;
+    vector<int> count(long long n)
+    {
+        vector<int> c(10);
         while(n)
         {
-            int r= n%10;
-            dig[r]++;
+            int r = n%10;
+            c[r]++;
             n/=10;
         }
-        // creating all power of 2
-        for(int i=0;i<32;i++)
+        return c;
+    }
+public:
+    bool reorderedPowerOf2(int n) {
+        vector<int> countOfN = count(n);
+        for(int i=0;i<31;i++)
         {
-            int x = 1<<i;
-            unordered_map<int,int> digPow;
-            while(x)
-            {
-                int xR = x%10;
-                digPow[xR]++;
-                x/=10;
-            }
-            bool flag = true;
-            // checking if this power digits equal to digits of n
-            for(auto x:digPow)
-            {
-                if(x.second!=dig[x.first])
-                {
-                    flag = false;
-                    break;
-                }   
-            }
-            for(auto x:dig)
-            {
-                if(x.second!=digPow[x.first])
-                {
-                    flag = false;
-                    break;
-                }   
-            }
-            // if flag is true digits are same
-            if(flag)
+            vector<int> countOfPow = count((1<<i));
+            if(countOfN==countOfPow)
                 return true;
         }
-        // Reach here mean no power of 2 have same digits as of n
         return false;
     }
 };
