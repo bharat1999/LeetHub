931029ca3bc52ab6060f5e6bbc6b7c5fd4103999
Bharat Malik <54993271+bharat1999@users.noreply.github.com>
2022-09-06 23:03:38 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (21.01%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -14,22 +14,18 @@ class Solution {
     {
         if(!root)
             return false;
-        bool flag = false;
         bool l = solve(root->left);
         bool r = solve(root->right);
-        if(root->val==1)
-            flag = true;
         if(!l)
             root->left = NULL;
         if(!r)
             root->right = NULL;
-        return flag or l or r;
+        return root->val==1 or l or r;
     }
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        solve(root);
-        if(!root->left and !root->right and root->val==0)
-            return NULL;
-        return root;
+        if(solve(root))
+            return root;
+        return NULL;
     }
 };
